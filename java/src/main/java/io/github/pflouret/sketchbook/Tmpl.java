2372eeb856097c651e971f4cef33507018cbdcef
Pablo Flouret <pflouret@gmail.com>
2017-03-17 23:33:46 -0700

    Tmpl.

--- java/src/main/java/io/github/pflouret/sketchbook/Tmpl.java
+++ java/src/main/java/io/github/pflouret/sketchbook/Tmpl.java
@@ -7,8 +7,21 @@ import java.lang.invoke.MethodHandles;
 
 public class Tmpl extends ProcessingApp {
     @Override
+    public void settings() {
+        super.settings();
+        //size(800, 800);
+        //size(800, 800, P3D);
+    }
+
+    @Override
     public void setup() {
         super.setup();
+
+        reset();
+    }
+
+    @Override
+    public void reset() {
     }
 
     @Override
